7cb14396392540496078802ccc58a931bad40c50
Loris Matteri <loris.matteri93@gmail.com>
2021-01-12 16:26:34 +0100

    Fa schifo, ma funziona
    al click sul button parte la richiesta all'API per i film contenenti la stringa inserita nell'input.

--- Milestone 1/assets/js/main.js
+++ Milestone 1/assets/js/main.js
@@ -1,1 +1,18 @@
-console.log('ciaone');
+let app = new Vue ({
+    el: '#app',
+    data: {
+        films: [],
+        searchString: "",
+    },
+    methods: {
+        cercaFilm: function (){
+            let searchContent = this.searchString
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key=ebf14a283df056252b947c3b861417f2&language=it-IT&query=" + searchContent)
+            .then(response => {
+                this.films = response.data.results
+                console.log(response.data.results);
+            });	
+            
+        }    
+    }
+})
