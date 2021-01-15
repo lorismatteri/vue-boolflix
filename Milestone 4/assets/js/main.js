ebfbbd3acf721a087db2047a74de59ddccc5e8e9
Loris Matteri <loris.matteri93@gmail.com>
2021-01-15 17:09:14 +0100

    Milestone 4 completamente completato
    accorciata ancora un po' la lunghezza dell'overview perchè in alcuni casi stabordava ancora. dopo inutili tentativi abbandonata l'idea di implementare l'hover in javascript e lo lasciamo al buon vecchio css con transition

--- Milestone 4/assets/js/main.js
+++ Milestone 4/assets/js/main.js
@@ -5,28 +5,18 @@ let app = new Vue ({
         tvSeries: [],
         searchString: "",
         langs: ['en', 'it', 'es', 'fr', 'de'],
-        visibility: 'no_visible'
     },
     methods: {
         cercaFilm: function (string){
             axios.get("https://api.themoviedb.org/3/search/movie?api_key=ebf14a283df056252b947c3b861417f2&language=it-IT&query=" + string)
             .then(response => {
                 this.films = response.data.results;
-                console.log(this.films);
-                //Per scoprire la lunghezza dell'overview
-                /* for (let i = 0; i < this.films.length; i++) {
-                    console.log(this.films[i].overview);
-                    let descr = this.films[i].overview;
-                    console.log(descr.length);
-                    
-                } */
-                
+                console.log(this.films);               
             });	
             axios.get("https://api.themoviedb.org/3/search/tv?api_key=ebf14a283df056252b947c3b861417f2&language=it-IT&query=" + string).then(response => {
                 this.tvSeries = response.data.results;
                 console.log(this.tvSeries);
-            });
-            
+            });           
         },
 
         getFlag: function(lang){
@@ -34,13 +24,7 @@ let app = new Vue ({
         },
         getCover: function(cover){
             return `https://image.tmdb.org/t/p/w342/${cover}`
-        },
-        hoverIn: function(){
-            this.visibility = 'visible'
-        },
-        hoverOut: function(){
-            this.visibility = 'no_visible'
-        }
+        },   
     },
 })
 
